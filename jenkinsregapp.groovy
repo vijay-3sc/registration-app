3977575a34bcae9d6de3569f9eb1ffcbd9d8b061
vijay_3sc <kanugula.vijay@3scsolution.com>
2024-08-20 13:04:36 +0530

    Update jenkinsregapp.groovy

--- jenkinsregapp.groovy
+++ jenkinsregapp.groovy
@@ -1,9 +1,9 @@
 pipeline{
     agent any
     environment{
-        ACR_AUTH = credentials('acr_creds')
-        ACR_LOGIN_SERVER = 'vijay.azurecr.io'
-        REGISTRY_NAME = 'vijay'
+        DOCKER_CREDENTIALS_ID = credentials('docker_creds')
+        DOCKER_REGISTRY_URL= 'https://hub.docker.com/u/vijay3sc'
+        REGISTRY_NAME = 'vijay3sc'
         REPO_NAME = 'registraton'
     }
     stages{
@@ -15,40 +15,20 @@ pipeline{
         stage('build docker image'){
             steps{
                 sh 'docker buildx prune  --force'
-                sh 'docker build -t ${ACR_LOGIN_SERVER}/${REPO_NAME}:$BUILD_NUMBER .'
+                sh 'docker build -t ${DOCKER_REGISTRY_URL}/${REPO_NAME}:$BUILD_NUMBER .'
             }
         }
         stage('push image'){
             steps{
                 withCredentials([usernamePassword(credentialsId: 'acr_creds', passwordVariable: 'password', usernameVariable:'username')]){
-                sh 'docker login -u ${username} -p ${password} ${ACR_LOGIN_SERVER}'
-                sh 'docker push ${ACR_LOGIN_SERVER}/${REPO_NAME}:$BUILD_NUMBER'
+                sh 'docker login -u ${username} -p ${password} ${DOCKER_REGISTRY_URL}'
+                sh 'docker push ${DOCKER_REGISTRY_URL}/${REPO_NAME}:$BUILD_NUMBER'
 
                 }
                 
             }
         }
         
-        stage('Deploy image on VM') {
-            steps{
-                withCredentials(bindings: [sshUserPrivateKey(credentialsId: 'vm_ssh_credentials', \
-                                                             keyFileVariable: 'SSH_KEY_FOR_VM')]) {
-                    sshPublisher (
-                        alwaysPublishFromMaster: false, 
-                        continueOnError: true, 
-                        publishers: [
-                            sshPublisherDesc(
-                            configName: 'vm_dev_server', 
-                            verbose: true,
-                            transfers: [ 
-                                sshTransfer(  execCommand: "sudo docker login ${ACR_LOGIN_SERVER} -u ${ACR_AUTH_USR} -p ${ACR_AUTH_PSW} && sudo docker pull ${REGISTRY_NAME}.azurecr.io/${REPO_NAME}:$BUILD_NUMBER"),
-                                sshTransfer(  execCommand: "sudo docker run -dit -p 8000:8080 ${REGISTRY_NAME}.azurecr.io/${REPO_NAME}:$BUILD_NUMBER")
-                            ]
-                            )
-                        ]
-                    )
-                }
-            }
-        }
+    
     }
 }
